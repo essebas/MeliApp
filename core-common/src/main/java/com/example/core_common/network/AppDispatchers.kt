b4541918ca59b87e8126e12a8ef735fa3b84a2e3
Diego Sebastian Camargo Lopez <Diego.Camargo@scotiabankcolpatria.com>
2022-08-31 01:00:29 -0500

    Create network dir and define dispatchers that the application will be use. Create module to provide this dispatchers and inject in the class that need it

---
+++ core-common/src/main/java/com/example/core_common/network/AppDispatchers.kt
@@ -0,0 +1,12 @@
+package com.example.core_common.network
+
+import javax.inject.Qualifier
+import kotlin.annotation.AnnotationRetention.RUNTIME
+
+@Qualifier
+@Retention(RUNTIME)
+annotation class Dispatcher(val appDispatchers: AppDispatchers)
+
+enum class AppDispatchers {
+    IO
+}
---
+++ core-common/src/main/java/com/example/core_common/network/di/DispatcherModule.kt
@@ -0,0 +1,18 @@
+package com.example.core_common.network.di
+
+import com.example.core_common.network.AppDispatchers
+import com.example.core_common.network.Dispatcher
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DispatcherModule {
+    @Provides
+    @Dispatcher(AppDispatchers.IO)
+    fun providesIODispatcher(): CoroutineDispatcher = Dispatchers.IO
+}
